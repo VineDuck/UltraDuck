e82d254909788733e04c8ec59e824759c2df2943
Jimbo5431 <githubjimbo@bend.me.uk>
2024-06-07 03:29:51 +0100

    Update UltraDuck.user.js

--- UltraDuck.user.js
+++ UltraDuck.user.js
@@ -14,7 +14,7 @@
 // @require     ./hideitems.js
 // @require     ./keypress.js
 // @require     ./quacker.js
-// @version     0.05.1
+// @version     0.05.2
 // @description Hides items, auto refreshes, quacks and notifies.
 // ==/UserScript==
 
