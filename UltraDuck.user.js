c413ec8c9b23417db11ff367ecbf7a2c2622e5b8
Jimbo5431 <githubjimbo@bend.me.uk>
2024-10-25 00:28:08 +0100

    Update UltraDuck.user.js
    Catch errors, auto refresh

--- UltraDuck.user.js
+++ UltraDuck.user.js
@@ -21,7 +21,7 @@
 // @author      Jimbo
 // @description Finds new items, and quacks
 // @run-at      document-start
-// @version     1.1.2.3
+// @version     1.1.2.4
 // @icon        https://raw.githubusercontent.com/VineDuck/UltraDuck/main/rubber-duck.png
 // ==/UserScript==
 
