361b1733efeb9f0944d1c0bf95432a2f3369f4c3
Jimbo5431 <githubjimbo@bend.me.uk>
2025-05-08 10:13:35 +0100

    fix mobile

--- UltraDuck.user.js
+++ UltraDuck.user.js
@@ -22,7 +22,7 @@
 // @author      Jimbo
 // @description Finds new items, and quacks
 // @run-at      document-start
-// @version     1.1.2.8
+// @version     1.1.2.9
 // @icon        https://raw.githubusercontent.com/VineDuck/UltraDuck/main/rubber-duck.png
 // ==/UserScript==
 
